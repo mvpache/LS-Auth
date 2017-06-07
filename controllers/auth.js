c30b2739e82024c434ea648ab22b2eb07df398ec
Michael Pacheco <mpacheco@thelotent.com>
2017-06-06 18:32:27 -0700

    tues

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,19 @@
 const { User } = require('./models');
+const { requireSignIn } = require('../services/passport');
+const getTokenForUser = require('../services/token');
 
 const signUp = (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (err) return res.send(err);
-    user.checkPassword(req.body.password,(err, isMatch) => {
-      if (err) return res.send(err);
-      res.send(isMAtch);
-    });
-  });
+
 };
 
 const signIn = (req, res) => {
   // generate a JWT token if the username/password is valid
+  res.send({
+    token: getTokenForUser(req.user)
+  });
 };
 
 module.exports = (app) => {
   app.post('/signup', signUp);
-  app.post('/signin', signIn);
+  app.post('/signin', requireSignIn, signIn);
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,16 @@
+const { User } = require('../models');
+
+
 const getUsers = (req, res) => {
   // return a list of all users if the provided JWT token is valid
+};
+
+const createUser = (req, res) => {
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) return res.send (err);
+    res.send(user);
+  });
 };
 
 module.exports = (app) => {
